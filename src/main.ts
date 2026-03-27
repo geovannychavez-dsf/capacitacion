a6daa5d7d05190ff3a01100d24d1971ef0b2cc74
Geovanny Chávez <geovannychavez@dispensariosagradafamilia.com.ec>
2026-03-27 17:04:22 -0500

    fix: se agrega permisos de cors
    se agrega permisos de cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ConfigService } from '@nestjs/config';
 import { TOKENSENV } from './common/types/type-orm';
 import * as cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter';
-
+import { CorsOptions } from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
@@ -20,11 +20,25 @@ async function bootstrap() {
       transform: true,
     }),
   );
-    app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type',
-  });
+const allowedOrigins = new Set([
+  'http://localhost:3001',
+  'http://localhost:5173'
+]);
+
+const corsOptions: CorsOptions = {
+  origin: (origin: string, callback: (error: Error | null, allowed: boolean) => void) => {
+    if (!origin || allowedOrigins.has(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,    
+};
+
+app.enableCors(corsOptions);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableVersioning({ type: VersioningType.URI });
   swaggerConsfig({
