cbd673acaa5d07921d936776e8b9e4b9a1c5de84
Geovanny Chávez <geovannychavez@dispensariosagradafamilia.com.ec>
2026-03-27 08:44:12 -0500

    fix: se agrega ApiUnauthorizedResponse  ApiInternalServerErrorResponse
    se agrega decoradores del swagger ApiInternalServerErrorResponse y ApiUnauthorizedResponse

--- src/modules/characters/decorator/put-character.decorator.ts
+++ src/modules/characters/decorator/put-character.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators, UsePipes, ValidationPipe } from "@nestjs/common";
-import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiOperation, ApiParam, getSchemaPath } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiParam, ApiUnauthorizedResponse, getSchemaPath } from "@nestjs/swagger";
 import { ResponseCharactersDto, UpdatCharactersDto } from "../dtos";
 
 export function putCharacterDecorator() {
@@ -21,7 +21,31 @@ export function putCharacterDecorator() {
                     message: 'Operacion exitosa',
                 },
             },
-        })
+        }),
+        ApiUnauthorizedResponse({
+            description: 'No tiene autorización',
+            schema: {
+                type: 'object',
+                properties: {
+                    statusCode: { type: 'number', example: 401 },
+                    message: {
+                        type: 'string',
+                        example: 'Credenciales incorrectas',
+                    },
+                    error: { type: 'string', example: 'Unauthorized' },
+                },
+            },
+        }),
+        ApiInternalServerErrorResponse({
+            description: 'Error interno del servidor',
+            schema: {
+                example: {
+                    statusCode: 500,
+                    message: 'Error interno del servidor',
+                    error: 'Internal Server Error',
+                },
+            },
+        }),
     );
 }
 
